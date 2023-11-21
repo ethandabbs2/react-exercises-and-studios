3a7b88dddefc7c76a00c2492b8ff5c4a5509b92e
ethandabbs2 <ethandabbs2@yahoo.com>
2023-11-20 22:59:08 -0600

    couple updates/practice

--- part1/exercises/src/App.js
+++ part1/exercises/src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import MovieList from './components/MovieList.js';
-import ChoresList from './components/ChoresList.js';
-import BookList from './components/BookList.js';
+import MovieList from './components/MovieList.jsx';
+import ChoresList from './components/ChoresList.jsx';
+import BookList from './components/BookList.jsx';
+import HobbyLinks from './components/HobbyLinks.jsx';
 
 function App() {
   return (
     <div className="App">
-      <MovieList />
       <ChoresList />
-      <BookList />
+      <div className="similarComponents">
+        <MovieList />
+        <BookList />
+        <HobbyLinks />
+      </div>
     </div>
   );
 }
